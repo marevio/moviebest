71cbf7b4b48809624301248a1acc6a764c451b56
Nik Psaragathos <nickpsaragathos@gmail.com>
2021-01-19 23:57:02 +0200

    bug fix it

--- moviebest/urls.py
+++ moviebest/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('aboutus/', views.aboutus, name='aboutus'),
     path('register/', views.Register, name='register'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
     path('logout/', views.logoutUser, name='logout')
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.contrib.auth import login , logout ,authenticate
+from django.contrib.auth import login as logins , logout ,authenticate
 from django.utils import timezone
 from .models import Movie
+
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -20,37 +22,47 @@ def aboutus(request):
 
 # Register
 def Register(request):
-    form = Register_form()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = Register_form()
 
-    if request.method == 'POST':
-        form = Register_form(request.POST)
-        if form.is_valid():
-            form.save()
+        if request.method == 'POST':
+            form = Register_form(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
 
-    context = {'form' : form}
-    return  render(request, 'movies/Register.html', context)
+        context = {'form' : form}
+        return  render(request, 'movies/Register.html', context)
 
 #login
 def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password =request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('movies/home.html')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
+            if user is not None:
+                logins(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
 
-    context = {}
-    return render(request, 'movie/login.html', context)
+        context = {}
+        return render(request, 'movies/login.html', context)
+
 
 #logout
 def logoutUser(request):
     logout(request)
-    return redirect(request, 'movie/login.html')
+    return redirect('home')
 
 #movies
 def movie_list(request):
@@ -69,6 +81,8 @@ def movie_list(request):
         posts = Movie.objects.filter(published_date__lte=timezone.now()).filter(category=cat).order_by('published_date')
     return render(request, 'movies/movie_list.html', {'movies': posts})
 
+#movie detail
+@login_required(login_url='login')
 def movie_detail(request, pk):
     post = get_object_or_404(Movie, pk=pk)
     return render(request, 'movies/movie_detail.html', {'movie': post})
